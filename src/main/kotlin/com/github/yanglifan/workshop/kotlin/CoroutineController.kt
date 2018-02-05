1714a420a2fc0fd7c9a3099f5b7459047016432f
Yang Lifan <yanglifan@gmail.com>
2018-02-05 15:00:59 +0800

    Add null process

--- src/main/kotlin/com/github/yanglifan/workshop/kotlin/CoroutineController.kt
+++ src/main/kotlin/com/github/yanglifan/workshop/kotlin/CoroutineController.kt
@@ -4,12 +4,17 @@ import com.github.yanglifan.workshop.kotlin.persistence.ReactiveAuditRepository
 import com.github.yanglifan.workshop.kotlin.persistence.ReactiveMessageRepository
 import com.github.yanglifan.workshop.kotlin.persistence.ReactivePeopleRepository
 import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.reactive.awaitFirst
+import kotlinx.coroutines.experimental.reactive.awaitFirstOrDefault
 import kotlinx.coroutines.experimental.reactive.awaitSingle
 import kotlinx.coroutines.experimental.reactor.mono
+import lombok.extern.slf4j.Slf4j
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.NoSuchElementException
 
+@Slf4j
 @RestController
 class CoroutineController(
         val peopleRepository: ReactivePeopleRepository,
@@ -17,7 +22,8 @@ class CoroutineController(
         val messageRepository: ReactiveMessageRepository) {
     @GetMapping("/coroutine/{personId}")
     fun getNumberOfMessages(@PathVariable personId: String) = mono(Unconfined) {
-        val person = peopleRepository.findById(personId).awaitSingle()
+        val person = peopleRepository.findById(personId).awaitFirstOrDefault(null)
+                ?: throw NoSuchElementException("No person can be found by $personId")
 
         val lastLoginDate = auditRepository.findByEmail(person.email).awaitSingle().eventDate
 
