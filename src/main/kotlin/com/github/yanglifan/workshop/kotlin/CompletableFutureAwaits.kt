10222671c200d4b26126dc3eca92413badd44d81
Yang Lifan <yanglifan@gmail.com>
2018-02-17 15:18:43 +0800

    1. Add completable future await sample 2. Add Kotlin Coroutine byte code sample

---
+++ src/main/kotlin/com/github/yanglifan/workshop/kotlin/CompletableFutureAwaits.kt
@@ -0,0 +1,15 @@
+package com.github.yanglifan.workshop.kotlin
+
+import java.util.concurrent.CompletableFuture
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.suspendCoroutine
+
+suspend fun <T> CompletableFuture<T>.await(): T =
+        suspendCoroutine<T> { cont: Continuation<T> ->
+            whenComplete { result, exception ->
+                if (exception == null) // the future has been completed normally
+                    cont.resume(result)
+                else // the future has completed with an exception
+                    cont.resumeWithException(exception)
+            }
+        }
